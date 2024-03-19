04c37b42e2401500800408775ff94e2b5769f233
minajeajeo <navercloudplatform.gmail.com>
2024-03-19 14:42:26 +0900

    회원가입과 연동한 server

--- backend/src/server/Sever.java
+++ backend/src/server/Sever.java
@@ -19,33 +19,33 @@ public class Sever extends WebSocketServer{
 	}
 
 	@Override
-	public void onClose(WebSocket arg0, int arg1, String arg2, boolean arg3) {
-		// TODO Auto-generated method stub
+	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+		System.out.println("연결 종료: " + conn.getRemoteSocketAddress());
 		
 	}
 
 	@Override
-	public void onError(WebSocket arg0, Exception arg1) {
-		// TODO Auto-generated method stub
+	public void onError(WebSocket conn, Exception ex) {
+		ex.printStackTrace();
 		
 	}
 
 	@Override
-	public void onMessage(WebSocket arg0, String arg1) {
-		// TODO Auto-generated method stub
+	public void onMessage(WebSocket conn, String message) {
+		System.out.println("메시지 수신: " + message);
+		// 여기서 메시지를 처리하고 클라이언트에게 응답을 보낼 수 있습니다.
 		
 	}
 
 	@Override
-	public void onOpen(WebSocket arg0, ClientHandshake arg1) {
-		// TODO Auto-generated method stub
+	public void onOpen(WebSocket conn, ClientHandshake handshake) {
+		System.out.println("새로운 연결: " + conn.getRemoteSocketAddress());
 		
 	}
 
 	@Override
 	public void onStart() {
-		// TODO Auto-generated method stub
-		
+		System.out.println("서버 시작");
 	}
 	
 }
