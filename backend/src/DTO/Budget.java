bc7650e9ef6010ef320b19d2a6d5803eb23b82fe
서혜리 <hye_dam_2@naver.com>
2024-03-19 10:54:51 +0900

    feat: 프로젝트 구조 및 buildpath, DTO 파일 작성

---
+++ backend/src/DTO/Budget.java
@@ -0,0 +1,38 @@
+package DTO;
+
+import java.sql.Date;
+
+public class Budget {
+	private Integer budgetId;
+	private Integer userId;
+	private Integer amount;
+	private Date month;
+	
+	//생성자는 필요할 때 추가할 예정
+	
+	public Integer getBudgetId() {
+		return budgetId;
+	}
+	public void setBudgetId(Integer budgetId) {
+		this.budgetId = budgetId;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public Integer getAmount() {
+		return amount;
+	}
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	public Date getMonth() {
+		return month;
+	}
+	public void setMonth(Date month) {
+		this.month = month;
+	}
+
+}
