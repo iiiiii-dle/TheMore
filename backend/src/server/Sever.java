3787fa1503c63b7ddff5db649e24298021fcd234
minajeajeo <navercloudplatform.gmail.com>
2024-03-19 13:56:51 +0900

    ServerMemebership

--- backend/src/server/Sever.java
+++ backend/src/server/Sever.java
@@ -1,5 +1,51 @@
 package server;
 
-public class Sever {
+import java.net.InetSocketAddress;
 
+import org.java_websocket.WebSocket;
+import org.java_websocket.handshake.ClientHandshake;
+import org.java_websocket.server.WebSocketServer;
+
+public class Sever extends WebSocketServer{
+	public static void main(String[] args) {
+		String host = "127.0.0.1";	// localhost
+		final int PORT = 9000;
+		
+		WebSocketServer server = new Sever(new InetSocketAddress(host, PORT));
+		server.run();
+	}
+	public Sever(InetSocketAddress inetAddr) {
+		super(inetAddr);
+	}
+
+	@Override
+	public void onClose(WebSocket arg0, int arg1, String arg2, boolean arg3) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onError(WebSocket arg0, Exception arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onMessage(WebSocket arg0, String arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onOpen(WebSocket arg0, ClientHandshake arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
