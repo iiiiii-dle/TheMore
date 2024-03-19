f81e16d941c9870726e1118159d1c34ec6c19425
서혜리 <hye_dam_2@naver.com>
2024-03-19 10:13:00 +0900

    feat: DTO 클래스들에 필드 및 게터 세터 추가

--- TheMore_be/src/DTO/Category.java
+++ TheMore_be/src/DTO/Category.java
@@ -1,5 +1,22 @@
 package DTO;
 
 public class Category {
-
+	private Integer categoryId;
+	private String categoryName;
+	
+	//생성자는 필요할 때 추가할 예정
+	
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryName() {
+		return categoryName;
+	}
+	public void setCategoryName(String categoryName) {
+		this.categoryName = categoryName;
+	}
+	
 }
--- TheMore_be/src/DTO/Users.java
+++ TheMore_be/src/DTO/Users.java
@@ -1,5 +1,66 @@
 package DTO;
 
-public class Users {
+import java.sql.Date;
 
+public class Users {
+	private Integer userId;
+	private String email;
+	private String password;
+	private String salt;		//복호화 난수
+	private String nickName;
+	private Date joinDate;		//가입일
+	private Boolean isActive;	//계정활성여부(탈퇴 안 하면 1, 탈퇴 시 0)
+	private Boolean isHidden;	//정보공개여부(공개 시 0, 비공개 시 1)
+	
+	//생성자는 필요할 때 추가할 예정
+	
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getSalt() {
+		return salt;
+	}
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public Date getJoinDate() {
+		return joinDate;
+	}
+	public void setJoinDate(Date joinDate) {
+		this.joinDate = joinDate;
+	}
+	public Boolean getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+	public Boolean getIsHidden() {
+		return isHidden;
+	}
+	public void setIsHidden(Boolean isHidden) {
+		this.isHidden = isHidden;
+	}
+	
 }
