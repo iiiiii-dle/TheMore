f517f995a6e9608b46a62121cbc81e7c8760e1a1
KangHyun_Kim <162311939+hmmumm@users.noreply.github.com>
2024-03-19 14:20:46 +0900

    main.js 작업 시작

---
+++ frontend/js/main.js
@@ -0,0 +1,26 @@
+document.addEventListener(("DOMContentLoaded",function(){
+    const text1 = document.querySelector(".text1");
+    const text2 = document.querySelector(".text2");
+    const text3 = document.querySelector(".text3");
+    const home_img = document.querySelector(".home_img");
+    const h2 = document.querySelector("h2");
+    const n1 = document.querySelector(".n1");
+    const n2 = document.querySelector(".n2");
+    const n3 = document.querySelector(".n3");
+    const n4 = document.querySelector(".n4");
+    const n5 = document.querySelector(".n5");
+    const n6 = document.querySelector(".n6");
+
+    text1.classList.add("aos-animate");
+    text2.classList.add("aos-animate");
+    text3.classList.add("aos-animate");
+    home_img.classList.add("aos-animate");
+    h2.classList.add("aos-animate");
+    n1.classList.add("aos-animate");
+    n1.classList.add("aos-animate");
+    n1.classList.add("aos-animate");
+    n1.classList.add("aos-animate");
+    n1.classList.add("aos-animate");
+
+    
+}))
