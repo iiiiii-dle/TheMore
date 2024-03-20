b2d4f3ab50b211452b17311481fa1be6e8799dac
maruduke <abab1117@naver.com>
2024-03-20 17:11:11 +0900

    LoginService 임시 생성

---
+++ backend/src/Service/LoginService.java
@@ -0,0 +1,32 @@
+package Service;
+
+import java.util.Map;
+
+import org.java_websocket.WebSocket;
+import org.json.JSONObject;
+
+import DAO.UsersDAO;
+import DB.DBConnection;
+import DTO.Users;
+
+public class LoginService {
+
+	public void Login(WebSocket socket,JSONObject json, Map<WebSocket, Users> session) {
+		
+		// Login Service 임시 추가
+		Users user;
+		try {
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+}
