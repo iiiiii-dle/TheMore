f2c9c24a93a52774ef8c33360341da9bedf05379
서혜리 <hye_dam_2@naver.com>
2024-03-20 21:54:05 +0900

    fix: package 이름 소문자로 변경

--- backend/src/server/Sever.java
+++ backend/src/server/Sever.java
@@ -5,16 +5,23 @@ import java.net.InetSocketAddress;
 import org.java_websocket.WebSocket;
 import org.java_websocket.handshake.ClientHandshake;
 import org.java_websocket.server.WebSocketServer;
+import org.json.JSONException;
 import org.json.JSONObject;
 
-public class Sever extends WebSocketServer{
+import service.ExpensesServiceImpl;
+
+public class Sever extends WebSocketServer {
+	
+	ExpensesServiceImpl expensesServiceImpl = new ExpensesServiceImpl();
+	
 	public static void main(String[] args) {
-		String host = "127.0.0.1";	// localhost
+		String host = "192.168.0.73";
 		final int PORT = 9000;
-		
+
 		WebSocketServer server = new Sever(new InetSocketAddress(host, PORT));
 		server.run();
 	}
+
 	public Sever(InetSocketAddress inetAddr) {
 		super(inetAddr);
 	}
@@ -22,41 +29,77 @@ public class Sever extends WebSocketServer{
 	@Override
 	public void onClose(WebSocket conn, int code, String reason, boolean remote) {
 		System.out.println("연결 종료: " + conn.getRemoteSocketAddress());
-		
+
 	}
 
 	@Override
 	public void onError(WebSocket conn, Exception ex) {
 		ex.printStackTrace();
-		
+
 	}
 
 	@Override
 	public void onMessage(WebSocket conn, String message) {
 		System.out.println("메시지 수신: " + message);
 		// 여기서 메시지를 처리하고 클라이언트에게 응답을 보낼 수 있습니다.
-		
+
+		// 혜리--------------------------------------------------
 		// 클라이언트로브터 받은 JSON 형식의 데이터를 파싱
-		JSONObject json = new JSONObject(message);
-		
+		JSONObject msgObj;
+		try {
+			msgObj = new JSONObject(message);
+		} catch(JSONException e) {
+			System.err.println("Invalid JSON format: " + e.getMessage());
+			return;
+		}
+		String cmd = msgObj.getString("cmd");
+
+		if (cmd.equals("join")) {
+			System.out.println("----------회원가입----------");
+			
+			// 구현 내용 넣어주세요
+			
+		} else if (cmd.equals("insertExpenses")) {
+			System.out.println("----------수입/지출 내역 작성----------");
+			ExpensesServiceImpl expensesService = new ExpensesServiceImpl();
+			expensesService.insertExpenses(conn, message);
+			
+		} else if (cmd.equals("deleteExpenses")) {
+			System.out.println("----------수입/지출 내역 삭제----------");
+			ExpensesServiceImpl expensesService = new ExpensesServiceImpl();
+			expensesService.deleteExpenses(conn, message);
+			
+		} else if (cmd.equals("updateExpenses")) {
+			System.out.println("----------수입/지출 내역 수정----------");
+			ExpensesServiceImpl expensesService = new ExpensesServiceImpl();
+			expensesService.updateExpenses(conn, message);
+			
+		} else if (cmd.equals("getExpensesList")) {
+			System.out.println("----------수입/지출 내역 조회----------");
+			ExpensesServiceImpl expensesService = new ExpensesServiceImpl();
+			expensesService.getExpensesList(conn, message);
+			
+		}
+
+		// 민재--------------------------------------------------
 		// 데이터베이스 업데이트 등의 작업 수행
-		String email = json.getString("email");
-		String nickName = json.getString("nickName");
-		String password = json.getString("password");
-		boolean isHidden = json.getBoolean("isHidden");
-		
+		String email = msgObj.getString("email");
+		String nickName = msgObj.getString("nickName");
+		String password = msgObj.getString("password");
+		boolean isHidden = msgObj.getBoolean("isHidden");
+
 		conn.send("정보가 성공적으로 업데이트되었습니다.");
 	}
 
 	@Override
 	public void onOpen(WebSocket conn, ClientHandshake handshake) {
 		System.out.println("새로운 연결: " + conn.getRemoteSocketAddress());
-		
+
 	}
 
 	@Override
 	public void onStart() {
 		System.out.println("서버 시작");
 	}
-	
+
 }
