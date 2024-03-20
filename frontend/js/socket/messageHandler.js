3b1de7a2e0047eabe1e3b40c75e979a25c8ea3af
maruduke <abab1117@naver.com>
2024-03-20 09:57:35 +0900

    WebSocket connect

--- frontend/js/socket/messageHandler.js
+++ frontend/js/socket/messageHandler.js
@@ -1,14 +1,12 @@
-import { ChatSocket } from './chatSocket.js';
 import { Chat_log, Message } from '../chat_log.js';
 
 // 실시간 Socket event 처리 파일
 
 function initialize() {
-    const host = 'ws://localhost';
+    const host = 'localhost';
     const port = 8080;
 
-    const chat_log = new Chat_log();
-    const chatSocket = new ChatSocket(host, port);
+    const chat_log = new Chat_log(host, port);
 }
 
 // 메시지 핸들러 함수
