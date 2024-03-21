add73ffb804a84efb2317822d721a23bb0e92cd4
서혜리 <hye_dam_2@naver.com>
2024-03-21 16:51:54 +0900

    fix: 중괄호 삭제

--- backend/src/server/Sever.java
+++ backend/src/server/Sever.java
@@ -59,11 +59,10 @@ public class Sever extends WebSocketServer {
       // 경석--------------------------------------------------
       if(cmd.equals("User")) {
          userService.parse(conn, msg, sessionUser);
-      }      
+    
       // 혜리--------------------------------------------------
       } else if (cmd.equals("Expenses")) {
     	  expensesService.parse(conn, msg);
-
       }
 //
 //      // 민재--------------------------------------------------
