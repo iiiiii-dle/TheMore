4ff1d6d0dee95d8e82fe7a063ef6a736764764c0
서혜리 <hye_dam_2@naver.com>
2024-03-22 19:03:35 +0900

    fix: account.js 수입지출 데이터 값 들어가도록 수정

--- frontend/js/account.js
+++ frontend/js/account.js
@@ -1,12 +1,36 @@
 import { Socket } from "../js/socket/socket.js";
 
+class Quote {
+    constructor() {
+        this.quote = document.querySelector('#quote');
+        this.quotes = [
+            '"한 푼 아낀 것은 한 푼 <br> 번 것이나 마찬가지다." <br> <br> - 벤자민 프랭클린',
+            '"버는 것보다 적게 쓰는 것을 안다면 <br> 현자의 돌을 가진 것과 같다." <br> <br> - 벤자민 프랭클린',
+            '"절약하지 않는 자는 <br> 고뇌하게 될 것이다." <br> <br> - 공자',
+            '"돈 생각을 떨쳐내는 유일한 <br> 방법은 돈을 많이 갖는 것이다." <br> <br> - 이디스 워튼',
+            '"가난에 대한 두려움이 삶을 <br> 지배하도록 내버려두면, <br> 그 대가로 당신은 먹기는 할 것이나 <br> 살지는 못할 것이다." <br> <br> - 조지 버나드쇼',
+            '"돈에 대해 쉽게 사는 방법은 <br> 생활수준을 형편보다 <br> 한 단계 낮추는 것이다." <br> <br> - 헨리 테일러 경',
+            '"돈이 전부는 아니지만, <br> 그만한 게 없다." <br> <br> - 메이웨더',
+            '"돈지갑의 밑바닥이 드러났을 때의 <br> 절약은 이미 늦은 행위다." <br> <br> - 세네카',
+            '"검약은 멋진 수입이다." <br> <br> - 에라스무스',
+            '"근면은 부유의 오른손이요, 절약은 그 왼손이다." <br> <br> - J.레이'
+        ];
+
+        this.updateQuote();
+    }
+    updateQuote() {
+        const randomQuote = this.quotes[Math.floor(Math.random() * this.quotes.length)];
+        this.quote.innerHTML = randomQuote;
+    }
+}
+
 class Calendar {
     constructor(socket) {
         this.socket = socket;
 
         this.quote = new Quote();
-        this.expensesBox = new ExpensesBox();
-        // DOM 요소 찾기
+        this.expensesBox = new ExpensesBox(socket);
+        
         this.calendar = document.querySelector('.calendar');
         this.calendarDays = document.querySelector('.calendar-days');
         this.calendarHeaderYear = document.querySelector('#year');
@@ -16,20 +40,16 @@ class Calendar {
         this.selectedDate = document.querySelector('#selectedDate');
         this.select = document.querySelector('#select');
 
-        // 현재 날짜 설정
         this.currentDate = new Date();
         this.currentMonth = this.currentDate.getMonth();
         this.currentYear = this.currentDate.getFullYear();
 
-        // 이벤트 리스너 추가
         document.querySelector('#pre-year').addEventListener('click', this.prevYear.bind(this));
         document.querySelector('#next-year').addEventListener('click', this.nextYear.bind(this));
         document.querySelector('#pre-month').addEventListener('click', this.prevMonth.bind(this));
         document.querySelector('#next-month').addEventListener('click', this.nextMonth.bind(this));
 
-        // 캘린더 생성
         this.generateCalendar(this.currentMonth, this.currentYear);
-        // 각 날짜 요소에 클릭 이벤트 추가
         this.calendarDays.addEventListener('click', this.handleDateClick.bind(this));
     }
 
@@ -109,7 +129,6 @@ class Calendar {
         }
     }
 
-
     // 각 날짜 클릭 시 처리
     handleDateClick(event) {
         const clickedDate = event.target.textContent;
@@ -139,34 +158,10 @@ class Calendar {
     }
 }
 
-class Quote {
-    constructor() {
-        this.quote = document.querySelector('#quote');
-        this.quotes = [
-            '"한 푼 아낀 것은 한 푼 <br> 번 것이나 마찬가지다." <br> <br> - 벤자민 프랭클린',
-            '"버는 것보다 적게 쓰는 것을 안다면 <br> 현자의 돌을 가진 것과 같다." <br> <br> - 벤자민 프랭클린',
-            '"절약하지 않는 자는 <br> 고뇌하게 될 것이다." <br> <br> - 공자',
-            '"돈 생각을 떨쳐내는 유일한 <br> 방법은 돈을 많이 갖는 것이다." <br> <br> - 이디스 워튼',
-            '"가난에 대한 두려움이 삶을 <br> 지배하도록 내버려두면, <br> 그 대가로 당신은 먹기는 할 것이나 <br> 살지는 못할 것이다." <br> <br> - 조지 버나드쇼',
-            '"돈에 대해 쉽게 사는 방법은 <br> 생활수준을 형편보다 <br> 한 단계 낮추는 것이다." <br> <br> - 헨리 테일러 경',
-            '"돈이 전부는 아니지만, <br> 그만한 게 없다." <br> <br> - 메이웨더',
-            '"돈지갑의 밑바닥이 드러났을 때의 <br> 절약은 이미 늦은 행위다." <br> <br> - 세네카',
-            '"검약은 멋진 수입이다." <br> <br> - 에라스무스',
-            '"근면은 부유의 오른손이요, 절약은 그 왼손이다." <br> <br> - J.레이'
-        ];
-
-        this.updateQuote();
-    }
-    updateQuote() {
-        const randomQuote = this.quotes[Math.floor(Math.random() * this.quotes.length)];
-        this.quote.innerHTML = randomQuote;
-    }
-}
-
 class ExpensesBox {
-    constructor(socket) {
+    constructor(socket, expenseAdd) {
         this.socket = socket;
-        this.expenseAdd = new ExpenseAdd();
+        this.expenseAdd = expenseAdd;
         this.expensesBox1 = document.querySelector('#expensesBox1');
         this.expenseList = document.querySelector('.expenseList');
         this.incomeLists = document.querySelector('.incomeLists');
@@ -188,7 +183,6 @@ class ExpensesBox {
         this.clickOutcomeBtn = this.clickOutcomeBtn.bind(this);
         this.clickIncomeBtn = this.clickIncomeBtn.bind(this);
 
-        // 요소가 제대로 찾아졌을 때 버튼 클릭 이벤트를 바인딩합니다.
         this.clickOutcomeBtn();
         this.clickIncomeBtn();
         this.clickAddListBtn();
@@ -198,13 +192,11 @@ class ExpensesBox {
     clickOutcomeBtn() {
         this.outcomeBtn.addEventListener('click', () => { // 화살표 함수로 변경
             this.expenseList.style.display = 'block';
-            // this.incomeLists.style.display = 'none';
         });
     }
 
     clickIncomeBtn() {
         this.incomeBtn.addEventListener('click', () => { // 화살표 함수로 변경
-            // this.incomeLists.style.display = 'block';
             this.expenseList.style.display = 'none';
         });
     }
@@ -212,14 +204,13 @@ class ExpensesBox {
     /* 채림 */
     clickAddListBtn() {
         this.addButton.addEventListener('click', () => {
-            // const expenseAddBoxStyle = getComputedStyle(this.expenseAdd);
             this.expensesBox1.style.display = 'none';
             this.expenseAdd.style.display = 'block';
-            this.categorys.classList.add('hidden'); // 안보이기
-            this.amount.classList.add('hidden'); // 안보이기
-            this.detail.classList.add('hidden'); // 안보이기
-            this.cancelBtn.classList.add('hidden'); // 안보이기
-            this.commitBtn.classList.add('hidden'); // 안보이기
+            this.categorys.classList.add('hidden');
+            this.amount.classList.add('hidden');
+            this.detail.classList.add('hidden');
+            this.cancelBtn.classList.add('hidden');
+            this.commitBtn.classList.add('hidden');
         });
     }
 }
@@ -227,7 +218,11 @@ class ExpensesBox {
 class ExpenseAdd {
     constructor(socket) {
         this.socket = socket;
-        this.quote = document.querySelector('#quote');
+        this.quote = new Quote();
+
+        this.categoryId = 9;
+        this.type = 0;
+
         this.expenseAdd = document.querySelector('.expenseAdd');
 
         this.budgetBtn = document.querySelector('.budgetBtn');
@@ -235,8 +230,9 @@ class ExpenseAdd {
         this.outcomeBtn = document.querySelector('#outcome');
         this.incomeCategoryGrid = document.querySelector('.incomeCategoryGrid');
         this.outcomeCategoryGrid = document.querySelector('.outcomeCategoryGrid');
-        this.categoryBtn = document.querySelector('.categoryBtn');
+        this.categoryBtn = document.querySelectorAll('.categoryBtn');
         this.select = document.querySelector('#select');
+        this.quoteDiv = document.querySelector('#quote');
 
         this.categorys = document.querySelector('.categorys');
         this.amount = document.querySelector('.amount');
@@ -248,19 +244,30 @@ class ExpenseAdd {
         this.clickIncomeBtn2 = this.clickIncomeBtn2.bind(this);
         this.clickOutcomeBtn2 = this.clickOutcomeBtn2.bind(this);
 
-        // this.clickCancelBtn = this.clickCancelBtn.bind(this);
+        this.commitBtn.addEventListener('click', () => {
+            this.submit();
+        });
 
+        this.incomeBtn.addEventListener('click', (e) => {
+            this.type = true;
+            this.categoryId = 14;
+        });
+        this.outcomeBtn.addEventListener('click', (e) => {
+            this.type = false;
+            this.categoryId = 9;
+        });
 
-        // this.incomeCategoryGrid = this.incomeCategoryGrid.bind(this);
-        // this.outcomeCategoryGrid = this.outcomeCategoryGrid.bind(this);
+        this.categoryBtn.forEach((element) => {
+            element.addEventListener('click', (event) => {
+                this.categoryId = event.target.getAttribute('id');
+            });
+        })
 
-        // 요소가 제대로 찾아졌을 때 버튼 클릭 이벤트를 바인딩합니다.
         this.clickBudgeBtn();
         this.clickIncomeBtn2();
         this.clickOutcomeBtn2();
         this.clickCancelBtn();
         this.clickCommitBtn();
-        this.submit();
     }
 
     clickBudgeBtn() {
@@ -302,7 +309,7 @@ class ExpenseAdd {
     clickCancelBtn() {
         this.cancelBtn.addEventListener('click', () => {
             this.expenseAdd.style.display = 'none';
-            this.quote.style.display = 'block';
+            this.quoteDiv.style.display = 'block';
             this.updateQuote();
         });
     }
@@ -310,77 +317,25 @@ class ExpenseAdd {
     clickCommitBtn() {
         this.commitBtn.addEventListener('click', () => {
             this.expenseAdd.style.display = 'none';
-            this.quote.style.display = 'block';
-            this.updateQuote();
+            this.quoteDiv.style.display = 'block';
+            this.quote.updateQuote();
         });
-    }
-
-    getUserId() {
-        return sessionStorage.getItem('userId');
-    }
-
-    getCategoryId() {
-        let categoryId;
-        this.categoryBtn.addEventListener('click', (e) => {
-            categoryId = this.categoryBtn.getAttribute('id');
-        });
-        return categoryId;
-    }
-
-    getType() {
-        let type;
-        this.incomeBtn.addEventListener('click', (e) => {
-            type = 1;
-        });
-        this.outcomeBtn.addEventListener('click', (e) => {
-            type = 0;
-        });
-        return type;
-    }
-
-    getMoney() {
-        let money;
-        this.amount.addEventListener('input', (e) => {
-            money = this.amount.innerHTML;
-        });
-        return money;
-    }
-
-    getMemo() {
-        let memo;
-        this.detail.addEventListener('input', (e) => {
-            memo = this.detail.innerHTML;
-        })
-        return memo;
-    }
-
-    getSelectedDate() {
-        return this.select.innerHTML;
     }
 
     submit() {
-        const userId = this.getUserId();
-        const categoryId = this.getCategoryId();
-        const type = this.getType();
-        const money = this.getMoney();
-        const memo = this.getMemo();
-        const expensesDate = new Date(this.getSelectedDate());
+        const data = {
+            'cmd': 'Expenses',
+            'cmd2': 'insertExpenses',
+            'userId': sessionStorage.getItem('userId'),
+            'categoryId': this.categoryId,
+            'type': this.type,
+            'money': document.querySelector("#amount").value,
+            'memo': document.querySelector('#detail').value,
+            'expensesDate': this.select.textContent,
+        }
 
-        const formData = new FormData();
-        formData.append('userId', userId);
-        formData.append('categoryId', categoryId);
-        formData.append('type', type);
-        formData.append('money', money);
-        formData.append('memo', memo);
-        formData.append('expensesDate', expensesDate);
-
-        formData.append('cmd', 'insertExpenses');
-
-
-        this.commitBtn.addEventListener('click', function () {
-
-            this.socket.sendMessage(JSON.stringify(Object.fromEntries(formData)));
-        });
+        const jsonData = JSON.stringify(data);
+        this.socket.sendMessage(jsonData);
     }
 
     resultHandler(jsonData) {
@@ -406,10 +361,10 @@ class ExpenseAdd {
 }
 
 function initialize() {
-    const socket = new Socket("localhost", 9000);
+    const socket = new Socket("localhost", 9000, () => {});
 
     const expenseAdd = new ExpenseAdd(socket);
-    const expensesBox = new ExpensesBox(socket);
+    const expensesBox = new ExpensesBox(socket, expenseAdd);
     const calendar = new Calendar(socket);
     calendar.displayExpenseBox();
 }
